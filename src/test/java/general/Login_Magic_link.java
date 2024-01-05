fc31ee3822508a94ad0aee242fb84ba497008162
Purvish Pandya <purvish.pandya@bombaysoftwares.com>
2024-01-05 15:24:58 +0530

    run by purvish

--- src/test/java/general/Login_Magic_link.java
+++ src/test/java/general/Login_Magic_link.java
@@ -120,8 +120,7 @@ public class Login_Magic_link
 			driver.findElement(By.xpath("(//*[@resource-id='com.impossible_research.sandbox.starboard:id/head']) [1]"))
 					.click();
 
-			// wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.Button[@text='Allow']")))
-			// 		.click();
+			// wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.Button[@text='Allow']"))).click();
 		
 
 			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.TextView[@text='Home']")));
