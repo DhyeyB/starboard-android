c9c1634ca0acd76c4f6d0d16222d8166ce4ae950
bhattasanadhyey <dhyey.bhattasana@bombaysoftwares.com>
2024-01-09 14:43:08 +0530

    deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@
 sudo apt install openjdk-8-jdk
 export JAVA_HOME=/usr/lib/jvm/temurin-8-jdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
+echo $JAVA_HOME
 
 # cat ~/.bashrc
 
@@ -36,6 +37,9 @@ sudo ls $ANDROID_SDK_ROOT
 
 sudo ls /usr/local/lib/android/sdk/tools
 sudo yes | /usr/local/lib/android/sdk/tools/bin/sdkmanager --licenses
+sudo /usr/local/lib/android/sdk/tools/bin/sdkmanager --update
+sudo /usr/local/lib/android/sdk/tools/bin/sdkmanager "extras;android;m2repository" "extras;google;m2repository"
+
 # sudo /usr/local/lib/android/sdk/tools/bin/sdkmanager --list | grep system-images
 # sudo /usr/local/lib/android/sdk/tools/bin/sdkmanager "platform-tools" "platforms;android-33"  "build-tools;30.0.3" "emulator"
 # sudo /usr/local/lib/android/sdk/tools/bin/sdkmanager "system-images;android-33;google_apis;x86_64"
