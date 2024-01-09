09a361d7ef7c53c6fa6808741da0d9fb93e06f80
bhattasanadhyey <dhyey.bhattasana@bombaysoftwares.com>
2024-01-09 11:18:14 +0530

    deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ sudo apt install openjdk-8-jdk
 wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
 unzip sdk-tools-linux-4333796.zip -d Android
 
-export ANDROID_HOME=$PWD/Android
-export PATH=$PATH:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools
+# export ANDROID_HOME=$PWD/Android
+# export PATH=$PATH:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools
 
-la -la $ANDROID_HOME
-la -la $ANDROID_HOME/tools
-la -la $ANDROID_HOME/tools/bin
+# la -la $ANDROID_HOME
+# la -la $ANDROID_HOME/tools
+# la -la $ANDROID_HOME/tools/bin
