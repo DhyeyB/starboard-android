a12564968ec080c6c89c9c4357d038b764ee03d7
bhattasanadhyey <dhyey.bhattasana@bombaysoftwares.com>
2024-01-09 12:37:36 +0530

    deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -34,7 +34,9 @@ yes | /usr/local/lib/android/sdk/tools/bin/sdkmanager --licenses
 sudo /usr/local/lib/android/sdk/tools/bin/sdkmanager --list | grep system-images
 # sudo /usr/local/lib/android/sdk/tools/bin/sdkmanager "platform-tools" "platforms;android-33"  "build-tools;30.0.3" "emulator"
 # sudo /usr/local/lib/android/sdk/tools/bin/sdkmanager "system-images;android-33;google_apis;x86_64"
-/usr/local/lib/android/sdk/tools/bin/avdmanager create avd -n testavd -k "system-images;android-33;google_apis;x86_64"
+# /usr/local/lib/android/sdk/tools/bin/avdmanager create avd -n testavd -k "system-images;android-33;google_apis;x86_64"
+/usr/local/lib/android/sdk/emulator/emulator -list-avds
+
 # /usr/local/lib/android/sdk/tools/emulator -avd testavd -no-window -cores 6 &
 # adb devices
 
