d6be8d46ce1219b04d727419d15d3709eb489956
bhattasanadhyey <dhyey.bhattasana@bombaysoftwares.com>
2024-01-09 15:45:37 +0530

    deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,13 @@ sudo apt install openjdk-8-jdk
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
 echo $JAVA_HOME
-
-# $JAVA_HOME_8_X64/bin/java -version
-# export JAVA_HOME=$JAVA_HOME_8_X64
 java -version
-ls /usr/lib/jvm
-
-echo $ANDROID_SDK_ROOT
-ls $ANDROID_SDK_ROOT
-
-ls $ANDROID_SDK_ROOT/tools
 yes | $ANDROID_SDK_ROOT/tools/bin/sdkmanager --licenses
 # sudo $ANDROID_SDK_ROOT/tools/bin/sdkmanager --update
 # sudo $ANDROID_SDK_ROOT/tools/bin/sdkmanager "extras;android;m2repository" "extras;google;m2repository"
 
 $ANDROID_SDK_ROOT/tools/bin/sdkmanager --list | grep system-images
+$ANDROID_SDK_ROOT/tools/bin/sdkmanager "platform-tools" "platforms;android-33"  "build-tools;30.0.3" "emulator"
 java -version
 $ANDROID_SDK_ROOT/tools/bin/avdmanager create avd -n testavd -k "system-images;android-33;google_apis_playstore;x86_64" -f
 $ANDROID_SDK_ROOT/tools/emulator -list-avds
