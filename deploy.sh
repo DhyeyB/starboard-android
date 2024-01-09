cc9f15bd8a4c5545e30fba7c78822f6d87f138bd
bhattasanadhyey <dhyey.bhattasana@bombaysoftwares.com>
2024-01-09 16:08:58 +0530

    deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,14 @@ export PATH=$JAVA_HOME/bin:$PATH
 echo $JAVA_HOME
 java -version
 yes | $ANDROID_SDK_ROOT/tools/bin/sdkmanager --licenses
-# sudo $ANDROID_SDK_ROOT/tools/bin/sdkmanager --update
-# sudo $ANDROID_SDK_ROOT/tools/bin/sdkmanager "extras;android;m2repository" "extras;google;m2repository"
-
 $ANDROID_SDK_ROOT/tools/bin/sdkmanager --list | grep system-images
 java -version
 $ANDROID_SDK_ROOT/tools/bin/sdkmanager "system-images;android-33;google_apis;x86_64"
 echo "no" | $ANDROID_SDK_ROOT/tools/bin/avdmanager create avd -n testavd -k "system-images;android-33;google_apis;x86_64" -f
 $ANDROID_SDK_ROOT/tools/emulator -list-avds
-
-# /usr/local/lib/android/sdk/tools/emulator -avd testavd -no-window -cores 6 &
+$ANDROID_SDK_ROOT/emulator -avd testavd -no-window -cores 6 &
+sleep 15
+gnome-screenshot --file screenshot.png
 # adb devices
 
 
